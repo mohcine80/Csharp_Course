02c9728ac13b73bc46423ae8b301009c6f012b0b
Mohcine <Mohcine@LAPTOP-F96BNM3U>
2017-01-12 20:33:24 +0100

    Add project files.

---
+++ variable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar=7;
+            Console.WriteLine("der Variable ist = {0}", myVar);
+            Console.ReadKey();
+            
+        }
+    }
+}
